ba0a926bbeda4eb1b978a8f283a74f3ae42d87c0
Valeria Mart <valeriamartirosyan2025@gmail.com>
2026-04-08 03:53:39 +0400

    Clean up: move source files to lab8 folder and remove from root

--- BytecodeDefs.cpp
+++
@@ -1,9 +0,0 @@
-v#include "BytecodeDefs.h"
-
-std::vector<RoutineLayout> globalRoutines;
-std::map<std::string, int> routineMap;
-
-std::string getCommandName(CommandID id) {
-    static const std::string names[] = { "SET_VAL", "GET_VAR", "PUT_VAR", "ADD", "SUB", "MUL", "DIV" /*...*/ };
-    return names[(int)id];
-}
--- SyntaxEngine.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "NodeStructure.h"
-#include <memory>
-
-std::unique_ptr<ExpressionNode> constructAST();s
